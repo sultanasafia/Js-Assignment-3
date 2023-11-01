fbcd33c947a978cf3955cea7ed96e9beca3bcde7
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-01 11:01:44 +0500

    js-assignment-3

---
+++ app.js
@@ -0,0 +1,21 @@
+// Q1============
+// let age = 15;
+// alert(`I am ${age} years old`)
+// Q2============
+// let visit = 14;
+// alert("You have visited this site "+visit+ " times")
+// Q3=============
+// let birthYear = 1991;
+// document.write(`My birth year is ${birthYear}`)
+
+// document.write("<br>")
+// document.write("Datatype of my declared variable is "+typeof birthYear)
+// Q4=================
+// let visiterName = "Jhon Doe"
+// let prducts = " T-shirt(s) "
+// let quantity = 5
+// let storeName = " XYZ Clothing store "
+// document.write(`${visiterName} ordered ${quantity} ${prducts} on ${storeName}`)
+
+
+// Js-Assignment copmpleted==================
